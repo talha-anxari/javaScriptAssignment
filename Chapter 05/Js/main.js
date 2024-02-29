52131815c2f0177c238630167f8b23edb7d1b2f5
talha-anxari <talhaanxaritn@outlook.com>
2024-02-29 22:59:00 +0500

    chapter 06

---
+++ Chapter 05/Js/main.js
@@ -0,0 +1,85 @@
+var a = 3;
+var b = 5;
+var c = a + b;
+
+console.log(c);
+
+var a = 5;
+var b = 3;
+var c = a - b;
+
+console.log(c);
+
+var a = 5;
+var b = 3;
+var c = a * b;
+
+console.log(c);
+
+var a = 5;
+var b = 3;
+var c = a / b;
+
+console.log(c);
+
+var fullName;
+console.log(fullName)
+
+// Step 1: Declare a variable
+// let myVariable;
+
+// Step 2: Show the value after variable declaration
+// console.log(`Value after variable declaration is: ${myVariable}`);
+
+// Step 3: Initialize the variable with some number
+// myVariable = 5;
+
+// Step 4: Show the initial value
+// console.log(`Initial value: ${myVariable}`);
+
+// Step 5: Increment the variable
+// myVariable++;
+
+// Step 6: Show the value after increment
+// console.log(`Value after increment is: ${myVariable}`);
+
+// Step 7: Add 7 to the variable
+// myVariable += 7;
+
+// Step 8: Show the value after adding 7
+// console.log(`Value after adding 7 is: ${myVariable}`);
+
+var ticket = 600;
+var five = 5;
+
+console.log(`Total coast of buy ticket to a movie is:`, five*ticket,`PKR`);
+
+console.log(`4 x 1 =`, 4*1);
+console.log(`4 x 2 =`, 4*2);
+console.log(`4 x 3 =`, 4*3);
+console.log(`4 x 4 =`, 4*4);
+console.log(`4 x 5 =`, 4*5);
+console.log(`4 x 6 =`, 4*6);
+console.log(`4 x 7 =`, 4*7);
+console.log(`4 x 8 =`, 4*8);
+console.log(`4 x 9 =`, 4*9);
+console.log(`4 x 10 =`, 4*10);
+
+console.log(`Shopping Cart`);
+
+var item1 = 650;
+var quantity1 = 3;
+
+var item2 = 100;
+var quantity2 = 7;
+
+console.log(`Total Cost Of Your Order is:`, item1*quantity1+ item2*quantity2);
+
+console.log(`Marks Sheet`);
+
+var totalMarks = 980;
+var obtainMarks = 804;
+
+var percentage = obtainMarks * 100 / totalMarks;
+
+console.log(percentage);
---
+++ Chapter 06/Js/main.js
@@ -0,0 +1,60 @@
+// Prefix Increment
+console.log(`Prefix Decrement`);
+var a = 11;
+var preNum = ++a;
+console.log('The Value Of A Is',a);
+console.log('Now The Value Of A Is',preNum);
+
+// Prefix Decrement
+console.log(`Prefix Increment`);
+var a = 11;
+var preNum = --a;
+console.log('Now The Value Of A Is',a);
+console.log('The Value Of A Is',preNum);
+
+// Post Increment
+console.log(`Post Increment`);
+var a = 11;
+var preNum = a++;
+console.log('The Value Of A Is',a)
+console.log('Now The Value Of A Is',preNum)
+
+// Post Decrement
+console.log(`Post Decrement`);
+var a = 11;
+var preNum = a--;
+console.log('Now The Value Of A Is',a);
+console.log('The Value Of A Is',preNum);
+
+
+var a = 2, b = 1;
+var result = --a - --b + ++b + b--;
+console.log(result);
+--a;
+console.log(a);
+--a - --b;
+console.log(a,b);
+--a - --b + ++b;
+console.log(a,b,b);
+--a - --b + ++b +b--;
+console.log(a,b,b);
+
+prompt(`Please enter your name`); 
+
+var five = prompt(`Enter Any Number`);
+alert(five*5);
+
+
+var urdu = 75;
+var social = 85;
+var Eng = 60;
+
+var totalMarks = 300;
+var obtainMarks = 220;
+
+var percentage = obtainMarks * 100 / totalMarks;
+alert(percentage);
+
+var end = `“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”`;
+document.write(end);
+
