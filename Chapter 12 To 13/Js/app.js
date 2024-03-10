be0e4360c850a9ee85701b01b15b082d1f91fabe
talha-anxari <talhaanxaritn@outlook.com>
2024-03-10 23:41:03 +0500

    chapter 12 to 16

---
+++ Chapter 12 To 13/Js/app.js
@@ -0,0 +1,64 @@
+// Question 02
+var num1, num2;
+
+if (parseInt(num1, 10) > parseInt(num2, 10)) {
+  console.log("The larger of " + num1 + " and " + num2 + " is " + num1 + ".");
+} else if (parseInt(num2, 10) > parseInt(num1, 10)) {
+  console.log("The larger of " + num1 + " and " + num2 + " is " + num2 + ".");
+} else {
+  console.log("The values " + num1 + " and " + num2 + " are equal.");
+}
+
+// Question 03
+var num = +prompt(`Please Enter A Number`);
+
+if(num > 0){
+    alert(`The Number Is Positive`)
+}else if(num < 0){
+    alert(`The Number Is Negative`)
+}else{
+    alert(`Your Number 0 Please Enter A Number Positive Or Negative`)
+}
+
+// Question 04
+var vowel = prompt(`Enter A Vowel Character`);
+if(vowel === 'a', 'e', 'i', 'o', 'u'){
+    alert(`true`)
+}else{
+    alert(`false Otherwise`)
+}
+
+// Question 05
+var originalPassword = `pakistan`;
+
+var userPassword = prompt(`Please Enter Your Password`)
+
+if(userPassword === originalPassword){
+    alert(`Your Password is Correct!`)
+}else{
+    alert(`Your Password Is Wrong`)
+}
+
+// Question 06
+var greeting;
+var hour = 13;
+if (hour < 18) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+
+// Question 07
+var userInput = prompt(`Enter a Time in 24-hours`);
+
+if(userInput >= '00' && userInput <= '04'){
+    alert(`Good Night`)
+}else if(userInput >= '05' && userInput <= '12'){
+    alert(`Good Noon`)
+}else if(userInput >= '13' && userInput <= '16'){
+    alert(`Good Afternoon`)
+}else if(userInput >= '17' && userInput <= '23'){
+    alert(`Good Evening`)
+}else{
+    alert(`Please Enter A Time`)
+};
---
+++ Chapter 14 To 16/Js/app.js
@@ -0,0 +1,38 @@
+// Question 01
+// var studentName = [];
+
+// Question 02 
+//  var studentName = new [];
+
+//  Question 03
+// var stringArray = [`Hello`, `World`, `Welcome To`, `Js Land`];
+
+// Question 04
+// var numberArray = [123, 456, 789,];
+
+// Question 05
+// var booleanArray = [true];
+
+// Question 06
+// var mixArray = [`Hello World`, 123, true];
+
+// Question 07
+var qualifications = ['SSC', 'HSC', 'BCS', 'BS', 'B.COM', 'MS', 'M.Phil.', 'PhD'];
+console.log(qualifications);
+
+// Question 08
+var stdName = [`Naveen`, `Daa`, `Hassan`];
+var ObtainMarks = [320, 230, 480];
+var totalMarks = 500;
+
+var percentage = ObtainMarks * 100 / totalMarks;
+console.log(percentage);
+
+// const students = ['Michael', 'John', 'Tony'];
+// const scores = [320, 230, 480];
+// const totalMarks = 500;
+
+// for (let i = 0; i < students.length; i++) {
+//     const percentage = (scores[i] / totalMarks) * 100;
+//     console.log(`Score of ${students[i]} is ${scores[i]}. Percentage: ${percentage.toFixed(2)}%`);
+// }
