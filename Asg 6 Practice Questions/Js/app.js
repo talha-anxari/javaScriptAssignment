888565de450c6646ca4e111378d28c5e763f14e6
talha-anxari <talhaanxaritn@outlook.com>
2024-03-21 19:55:23 +0500

    practice assigment

---
+++ Asg 6 Practice Questions/Js/app.js
@@ -0,0 +1,32 @@
+// //TODO: Question 01 
+// for(var i = 1; i < 11; i++){
+//     console.log(i);
+// }
+
+// //TODO: Question 02
+// var evenNum = [];
+// for(var i = 0; i <= 20; i += 2){
+//     evenNum.push(i);
+// }
+// console.log(evenNum);
+
+// for(var i = 1; i <= 10; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+// //TODO: Question 03
+// var oddNum = [];
+// for(var i = 1; i <= 19; i += 2){
+//     oddNum.push(i);
+// }
+// console.log(oddNum);
+
+// //TODO: Question 04
+
+// for(var i = 1; i <= 10; i++){
+//     var square = i * i;
+//     console.log(`The Square is ${i} is ${square}`);
+// }
+
